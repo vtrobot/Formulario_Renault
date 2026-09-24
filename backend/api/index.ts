da844ebc16096f7c1ba2503983a855c7ddfbd895
Valmor Tambosi Junior <valmor@outlook.com>
2026-09-24 17:24:41 -0300

    ajuste vercel

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,6 +1,7 @@
 import app from '../src/app';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: any, res: any) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   await app.ready();
   app.server.emit('request', req, res);
 }
